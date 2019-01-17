2d2cc943d2fbe529365473f27d83e07d4a140d47
Guillaume <occuly.guillaume@gmail.com>
2019-01-17 12:01:00 +0100

    Dynamic Routing & History Mode

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 import EventCreate from "./views/EventCreate.vue"
 import EventShow from "./views/EventShow.vue"
 import EventList from "./views/EventList.vue"
+import User from "./views/User.vue"
 
 Vue.use(Router)
 
@@ -16,14 +17,21 @@ export default new Router({
       component: EventList
     },
     {
-      path: "/event",
+      path: "/event/:id",
       name: "event-show",
-      component: EventShow
+      component: EventShow,
+      props: true
     },
     {
       path: "/event/create",
       name: "event-create",
       component: EventCreate
+    },
+    {
+      path: "/user/:username",
+      name: "user",
+      component: User,
+      props: true
     }
   ]
 })
