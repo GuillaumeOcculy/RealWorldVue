85c157ce4ba12f0da5b2b19613fd4fac8c9322cb
Guillaume <occuly.guillaume@gmail.com>
2019-02-03 21:39:06 +0100

    Mutations & Actions Pt. 2

--- src/store.js
+++ src/store.js
@@ -22,16 +22,22 @@ export default new Vuex.Store({
       { id: 3, text: "...", done: true },
       { id: 4, text: "...", done: false }
     ],
-    events: [
-      { id: 1, title: "...", organizer: "..." },
-      { id: 2, title: "...", organizer: "..." },
-      { id: 3, title: "...", organizer: "..." },
-      { id: 4, title: "...", organizer: "..." }
-    ]
+    events: [],
+    eventsTotal: 0,
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS_TOTAL(state, total) {
+      state.eventsTotal = total
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -39,20 +45,36 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event)
       })
+    },
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id)
+
+      if (event) {
+        commit("SET_EVENT", event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit("SET_EVENT", response.data)
+          })
+          .catch(error => {
+            console.log("There was an error:", error.response)
+          })
+      }
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit("SET_EVENTS_TOTAL", response.headers["x-total-count"])
+          commit("SET_EVENTS", response.data)
+        })
+        .catch(error => {
+          console.log("There was an error:", error.response)
+        })
     }
   },
   getters: {
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
-    },
-    catLength: state => {
-      return state.categories.length
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done)
-    },
-    activeTodosCount: state => {
-      return state.todos.filter(todo => !todo.done).length
     }
   }
 })
